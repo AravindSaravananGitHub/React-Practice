7974204728d1bde2c0dc00d570db9ce1a39ccf6e
Aravind S <aravindprime123@gmail.com>
2025-03-17 06:43:03 +0530

    Practicing useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const games = ["Cricket", "Mobile games", "football", "Running race"];
+  const [game, setGame] = useState(games[0]);
+
+  const changeName = () => {
+    const randomNumber = Math.floor(Math.random() * games.length);
+    setGame(games[randomNumber]);
+  };
   return (
     <div>
-      <h1>Hello World !</h1>
+      <h1>I am good in {game}</h1>
+      <button onClick={changeName}>Change</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
