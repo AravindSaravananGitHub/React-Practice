a5f1baf649d90d79c8be85e53dcb113fc86d5621
Aravind S <aravindprime123@gmail.com>
2025-03-26 07:08:40 +0530

    First component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
+import Header from "./Header";
 
 const App = () => {
-  const games = ["Cricket", "Mobile games", "football", "Running race"];
-  const [game, setGame] = useState(games[0]);
-
-  const changeName = () => {
-    const randomNumber = Math.floor(Math.random() * games.length);
-    setGame(games[randomNumber]);
-  };
   return (
     <div>
-      <h1>I am good in {game}</h1>
-      <button onClick={changeName}>Change</button>
+      <Header />
     </div>
   );
 };
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="heading">
+        <h1>Do To List</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
