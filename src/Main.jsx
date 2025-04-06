93a841a37a5233141c1423716e62e0bf0f577b10
Aravind S <aravindprime123@gmail.com>
2025-04-06 09:02:26 +0530

    updated the list

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Main = () => {
-  return <div className="main">Main</div>;
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Cricket",
+    },
+    {
+      id: 1,
+      checked: true,
+      item: "PS4",
+    },
+    {
+      id: 1,
+      checked: false,
+      item: "Learning React",
+    },
+  ]);
+  return (
+    <div className="main">
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              className="checkBox"
+              type="checkbox"
+              checked={item.checked}
+            />
+            <label htmlFor="checkBox">{item.item}</label>
+            <FaTrashAlt role="button" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Main;
